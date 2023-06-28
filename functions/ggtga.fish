5ab30fb37a01574398885c17db16285d6e4c735a
Fung Wing Kit <wengee@gmail.com>
2023-06-28 14:56:13 +0800

    commit

--- functions/ggtga.fish
+++ functions/ggtga.fish
@@ -1,8 +1,7 @@
 function ggtga --description 'Commit all changes, auto tag, push the tag to remote'
     if ggpa
         set VERSION (date +'%Y%m%d%H%M')
-        echo $VERSION
-        # git tag $VERSION
-        # git push origin $VERSION
+        git tag $VERSION
+        git push origin $VERSION
     end
 end
