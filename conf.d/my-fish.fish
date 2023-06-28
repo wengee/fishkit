250b00fa9dfb48e159d843d41604e4f4c08d33df
Fung Wing Kit <wengee@gmail.com>
2023-06-28 14:25:38 +0800

    update

--- conf.d/my-fish.fish
+++ conf.d/my-fish.fish
@@ -2,4 +2,15 @@ if status is-interactive
     if status is-login
         cd $HOME
     end
+
+    alias notepad="/mnt/c/Windows/notepad.exe"
+    alias open="/mnt/c/Windows/explorer.exe"
+    alias code="/mnt/d/Program\ Files/VSCode/bin/code"
+
+    alias root="sudo su -"
+    alias vi=vim
+    alias initwsl="sudo /etc/wsl.init"
+    alias redis=redis-cli
+
+    set -gx EDITOR vim
 end
