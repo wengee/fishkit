8214fffa5a3483df776c4118608fdbb713d4ff78
Fung Wing Kit <wengee@gmail.com>
2023-07-06 16:30:16 +0800

    update

--- functions/__fishkit.init.fish
+++ functions/__fishkit.init.fish
@@ -20,6 +20,12 @@ function __fishkit.init
         alias myip="curl http://myip.ipip.net/"
     
         set -gx EDITOR vim
+
+        for p in /usr/local/bin /usr/local/sbin
+            if not contains $p $PATH
+                set -gx PATH $p $PATH
+            end
+        end
         set -gx PATH . $HOME/.local/bin $HOME/.yarn/bin $HOME/.config/composer/bin $HOME/.composer/vendor/bin $PATH
 
         abbr -a -g ggpull git pull origin \(__git.current_branch\)
